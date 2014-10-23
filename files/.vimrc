a7e1eb972c3b6989129bdf64ff50d86872617df2
John Rummel <jrummel@hostgator.com>
2014-10-23 11:33:08 -0500

    Basic env config via puppet

---
+++ files/.vimrc
@@ -0,0 +1,6 @@
+execute pathogen#infect()
+colorscheme vividchalk
+set number
+set smartindent
+set tabstop=4
+set shiftwidth=4
---
+++ manifests/init.pp
@@ -0,0 +1,33 @@
+class rummy {
+
+    $package = ['vim-common', 'vim-enhanced']
+
+    $user = 'rummy'
+
+    user { 
+        $user:
+            ensure => "present",
+            managehome => true
+    }
+
+    package { 
+        $package:
+            ensure => "installed"
+    } 
+
+    file {
+            "/home/${user}/.vimrc" :
+            source => "puppet:///modules/${user}/.vimrc",
+            require => [ Package['vim-enhanced'], User[$user] ]
+    }
+
+    file {
+            "/home/${user}/.vim" :
+            ensure => directory,
+            recurse => true,
+            purge => true,
+            source => "puppet:///modules/${user}/.vim",
+            require => Package['vim-enhanced']
+    }
+
+}
